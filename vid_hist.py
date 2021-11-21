55878ed9ddbe11a5b218a4edee85a087f4454cab
Brendan Dolan-Gavitt <brendandg@nyu.edu>
2021-11-20 23:23:34 -0500

    Add a bunch of debug and info logging

--- vid_hist.py
+++ vid_hist.py
@@ -3,6 +3,7 @@
 import cv2
 import sys
 import numpy as np
+import logging
 from pathlib import Path
 import matplotlib.pyplot as plt
 import squarify
@@ -47,12 +48,17 @@ parser.add_argument('-d', '--output_dir', default=None, help='Output directory')
 parser.add_argument('-t', '--threshold', type=int, default=0, help='Ignore colors that are within this distance of black/white (Euclidean)')
 # Ignore colors that are more than a particular percentage of the total
 parser.add_argument('-p', '--percent', type=float, default=100.0, help='Ignore colors that are more than this percent of the video')
-
+# Verbose flag
+parser.add_argument('-V', '--verbose', action="store_const", dest="loglevel", const=logging.INFO)
+# Debug flag
+parser.add_argument('-D', '--debug', action="store_const", dest="loglevel", const=logging.DEBUG)
 args = parser.parse_args()
 ClusterAlg = {
     'kmeans': KMeans,
     'mbkmeans': MiniBatchKMeans
 }[args.method]
+log_fmt = '[{levelname:<5s}] {message}'
+logging.basicConfig(level=args.loglevel, format=log_fmt, style='{')
 
 if args.percent > 100.0 or args.percent < 0.0:
     parser.error('Percentage must be between 0 and 100')
@@ -81,9 +87,19 @@ def iter_iframes(cap):
 BATCH_SIZE = 1000
 
 def get_hist_opencv(filename, iframes=False):
+    def val2chars(i):
+        return bytes.fromhex(hex(int(i))[2:])[::-1].decode()
+    cap = cv2.VideoCapture(filename)
+    pix_fmt = val2chars(cap.get(cv2.CAP_PROP_CODEC_PIXEL_FORMAT))
+    fourcc = val2chars(cap.get(cv2.CAP_PROP_FOURCC))
+    logging.debug(
+        f'Opened {filename} {int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))}x{int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))} '
+        f'{pix_fmt} {int(cap.get(cv2.CAP_PROP_FRAME_COUNT))} '
+        f'{cap.get(cv2.CAP_PROP_FPS)} fps {fourcc}')
     hist = np.zeros((256,256,256))
     framefn = iter_iframes if args.iframes else iter_frames
     i = 0
+    total_frames = 0
     batch = []
     for frame in framefn(cap):
         batch.append(frame)
@@ -91,8 +107,11 @@ def get_hist_opencv(filename, iframes=False):
         if i == BATCH_SIZE:
             hist += cv2.calcHist(batch, [0,1,2], None, [256,256,256], [0,256,0,256,0,256])
             batch = []
+            total_frames += i
             i = 0
     if batch: hist += cv2.calcHist(batch, [0,1,2], None, [256,256,256], [0,256,0,256,0,256])
+    total_frames += i
+    logging.debug(f'Processed {total_frames} frames')
     cap.release()
     return hist
 
@@ -100,9 +119,14 @@ def get_hist_pyav(filename, iframes=False):
     hist = np.zeros((256,256,256))
     with av.open(filename) as container:
         stream = container.streams.video[0]
+        logging.debug(
+            f'Opened {filename} {stream.codec_context.width}x{stream.codec_context.height} '
+            f'{stream.codec_context.pix_fmt} {stream.codec_context.encoded_frame_count} '
+            f'{float(stream.codec_context.framerate)} fps {stream.codec_context.codec.name}')
         if iframes: stream.codec_context.skip_frame = 'NONREF'
         stream.thread_type = 'AUTO'
         i = 0
+        total_frames = 0
         batch = []
         for frame in container.decode(stream):
             batch.append(frame.to_ndarray(format='rgb24'))
@@ -110,8 +134,11 @@ def get_hist_pyav(filename, iframes=False):
             if i == BATCH_SIZE:
                 hist += cv2.calcHist(batch, [0,1,2], None, [256,256,256], [0,256,0,256,0,256])
                 batch = []
+                total_frames += i
                 i = 0
         if batch: hist += cv2.calcHist(batch, [0,1,2], None, [256,256,256], [0,256,0,256,0,256])
+        total_frames += i
+        logging.debug(f'Processed {total_frames} frames')
     return hist
 
 if args.output_dir:
@@ -120,8 +147,7 @@ else:
     hist_filename = vid_filename.with_suffix('.hist.npz')
 
 if args.force or not hist_filename.exists():
-    cap = cv2.VideoCapture(str(vid_filename))
-    print("Calculating video histogram...")
+    logging.info("Calculating video histogram...")
     if args.backend == 'opencv':
         hist = get_hist_opencv(str(vid_filename),iframes=args.iframes)
     elif args.backend == 'pyav':
@@ -136,18 +162,21 @@ else:
 # Initial list of colors in the video
 all_colors = np.argwhere(hist).astype(np.uint8)
 
+logging.info(f"Total colors: {len(all_colors)}")
+
 # Filter out colors that are too close to black or white
 if args.threshold:
-    print("Filtering out colors that are too close to black...", end='', flush=True)
+    logging.info("Filtering out colors that are too close to black...")
     all_colors = all_colors[np.sqrt(np.sum(np.square(np.abs(all_colors - [0,0,0])), axis=1)) > args.threshold]
-    print(f"{len(all_colors)} colors remain")
-    print("Filtering out colors that are too close to white...", end='', flush=True)
+    logging.info(f"{len(all_colors)} colors remain")
+    logging.info("Filtering out colors that are too close to white...")
     all_colors = all_colors[np.sqrt(np.sum(np.square(np.abs(all_colors - [255,255,255])), axis=1)) > args.threshold]
-    print(f"{len(all_colors)} colors remain")
+    logging.info(f"{len(all_colors)} colors remain")
 
 # Use K-Means to cluster the colors
-print(f"Clustering to select {args.colors} dominant colors...")
-kmeans = ClusterAlg(n_clusters=args.colors, random_state=0)
+logging.info(f"Clustering to select {args.colors} dominant colors...")
+kmeans = ClusterAlg(n_clusters=args.colors, random_state=0,
+    verbose=1 if args.loglevel == logging.DEBUG else 0)
 kmeans.fit(all_colors, sample_weight=hist[all_colors[:,0],all_colors[:,1],all_colors[:,2]])
 palette = np.rint(kmeans.cluster_centers_).astype(np.uint8)
 # Convert backend's color format to RGB
@@ -160,11 +189,11 @@ palhist /= palhist.sum()
 
 # Filter out colors that are too dominant (more than a certain percentage of the video after clustering)
 if args.percent < 100.0:
-    print("Filtering out colors that are too dominant...", end='', flush=True)
+    logging.info("Filtering out colors that are too dominant...")
     before = len(palhist[palhist > 0.0])
     palhist[palhist*100.0 > args.percent] = 0.0
     after = len(palhist[palhist > 0.0])
-    print(f"palette reduced from {before} to {after} colors")
+    logging.info(f"Palette reduced from {before} to {after} colors")
 
 palette = palette.astype(np.float32) / 255
 # Use squarify to plot the histogram using the palette colors.
